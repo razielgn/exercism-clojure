dbad2fdf5f48d4cff5bb954489a2e5655a83dc9b
Federico Ravasio <ravasio.federico@gmail.com>
2015-05-04 16:59:12 +0200

    Improved anagram by using a predicate.

--- anagram/anagram.clj
+++ anagram/anagram.clj
@@ -1,13 +1,12 @@
 (ns anagram
   (:require [clojure.string :refer [lower-case]]))
 
-(defn- anagram-of? [word candidate]
-  (let [norm-word (lower-case word)
-        norm-candidate (lower-case candidate)]
-    (if (= norm-word norm-candidate)
-      false
-      (= (sort norm-word)
-         (sort norm-candidate)))))
-
 (defn anagrams-for [word candidates]
-  (filter #(anagram-of? word %1) candidates))
+  (let [word (lower-case word)
+        letters (sort word)
+        anagram? (fn [cand]
+                   (let [cand (lower-case cand)]
+                     (if (= word cand)
+                       false
+                       (= letters (sort cand)))))]
+    (filter anagram? candidates)))
