2e0d4f5e566f45f75f379136086fbee328b94346
Federico Ravasio <ravasio.federico@gmail.com>
2015-05-04 17:00:08 +0200

    Added beer-song.

---
+++ beer-song/beer_song.clj
@@ -0,0 +1,37 @@
+(ns beer-song
+  (:require [clojure.string :as s]))
+
+(defn- bottles-of-beer [n]
+  (condp = n
+    0 "no more bottles of beer"
+    1 "1 bottle of beer"
+    (str n " bottles of beer")))
+
+(defn- first-line [n]
+  (let [bottles (bottles-of-beer n)]
+    (format "%s on the wall, %s.\n"
+            (s/capitalize bottles)
+            bottles)))
+
+(defn- second-line [n]
+  (if (= n 0)
+    (format "Go to the store and buy some more, %s on the wall.\n"
+            (bottles-of-beer 99))
+    (let [bottles (bottles-of-beer (- n 1))
+          obj-complement (if (= n 1) "it" "one")]
+      (format "Take %s down and pass it around, %s on the wall.\n"
+              obj-complement
+              bottles))))
+
+(defn verse [n]
+  (str (first-line n)
+       (second-line n)))
+
+(defn sing
+  ([from]
+   (sing from 0))
+  ([from to]
+   (->> (range to (+ from 1))
+        reverse
+        (map verse)
+        (reduce #(str %1 "\n" %2)))))
