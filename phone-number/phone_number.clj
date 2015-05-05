f711ddc33c30664e3d4b507f4f430e00df94c55f
Federico Ravasio <ravasio.federico@gmail.com>
2015-05-05 21:12:31 +0200

    Added phone-number.

---
+++ phone-number/phone_number.clj
@@ -0,0 +1,40 @@
+(ns phone-number
+  (:require [clojure.string :as str]))
+
+(defn- char-digit? [ch]
+  (java.lang.Character/isDigit ch))
+
+(def invalid-number "0000000000")
+
+(defn number
+  "Removes non-numeric characters."
+  [string]
+  (let [digits (filter char-digit? string)]
+    (condp = (count digits)
+      10 (str/join digits)
+      11 (if (= \1 (first digits))
+           (str/join (rest digits))
+           invalid-number)
+      invalid-number)))
+
+(defn- break-number [no]
+  (let [area-code (subs no 0 3)
+        office-code (subs no 3 6)
+        line-code (subs no 6 10)]
+    (map str/join [area-code
+                   office-code
+                   line-code])))
+
+(defn area-code
+  "Returns a phone number's area code."
+  [string]
+  (let [no (number string)
+        [ac _ _] (break-number no)]
+    ac))
+
+(defn pretty-print
+  "Pretty prints a phone number with format `(555) 111-2222`."
+  [string]
+  (let [no (number string)
+        [ac oc lc] (break-number no)]
+    (str "(" ac ") " oc "-" lc)))
