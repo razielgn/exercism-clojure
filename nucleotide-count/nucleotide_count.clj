fe6995496864eb3131a52812ad88f179e4309a6a
Federico Ravasio <ravasio.federico@gmail.com>
2015-05-05 21:01:22 +0200

    Improved nucleotide-count by using `frequencies`.

--- nucleotide-count/nucleotide_count.clj
+++ nucleotide-count/nucleotide_count.clj
@@ -1,21 +1,18 @@
 (ns nucleotide-count
   (:require [clojure.core :as core]))
 
-(defn count
-  "Counts occurrences of a given nucleotide inside a DNA string."
-  [nucleotide dna-string]
-  (let [counter (fn [acc nucleotide']
-                  (if (= nucleotide nucleotide')
-                    (inc acc)
-                    acc))]
-    (if (contains? #{\A \T \C \G} nucleotide)
-      (reduce counter 0 dna-string)
-      (throw (Exception. "invalid nucleotide")))))
+(def nucleotide-counts-base
+  {\A 0 \T 0 \C 0 \G 0})
 
 (defn nucleotide-counts
   "Counts occurrences of each nucleotide inside a DNA string."
   [dna-string]
-  (let [start {\A 0 \T 0 \C 0 \G 0}
-        aggregator (fn [h nucleotide]
-                     (update-in h [nucleotide] inc))]
-    (reduce aggregator start dna-string)))
+  (merge nucleotide-counts-base
+         (frequencies dna-string)))
+
+(defn count
+  "Counts occurrences of a given nucleotide inside a DNA string."
+  [nucleotide dna-string]
+  (if (contains? #{\A \T \C \G} nucleotide)
+    ((nucleotide-counts dna-string) nucleotide)
+    (throw (Exception. "invalid nucleotide"))))
