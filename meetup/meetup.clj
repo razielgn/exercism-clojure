72195ad57439259b8a80010391f88551f1a21f31
Federico Ravasio <ravasio.federico@gmail.com>
2015-05-07 22:43:29 +0200

    Added meetup.

---
+++ meetup/meetup.clj
@@ -0,0 +1,36 @@
+(ns meetup
+  (:require [clj-time.core :as t]
+            [clj-time.predicates :as p]))
+
+(defn- predicate-for-day [weekday]
+  (condp = weekday
+    :monday p/monday?
+    :tuesday p/tuesday?
+    :wednesday p/wednesday?
+    :thursday p/thursday?
+    :friday p/friday?
+    :saturday p/saturday?
+    :sunday p/sunday?))
+
+(defn- find-day [schedule candidates]
+  (condp = schedule
+    :first (first candidates)
+    :second (second candidates)
+    :third (nth candidates 2)
+    :fourth (nth candidates 3)
+    :teenth (first (drop-while #(< % 13) candidates))
+    :last (last candidates)))
+
+(defn- produce-candidates [year month weekday]
+  (let [start-date (t/first-day-of-the-month year month)
+        end-date (t/last-day-of-the-month year month)
+        interval (t/interval start-date end-date)]
+    (->> (range (inc (t/in-days interval)))
+         (map #(t/plus start-date (t/days %)))
+         (filter (predicate-for-day weekday))
+         (map t/day))))
+
+(defn meetup [month year weekday schedule]
+  (let [day (->> (produce-candidates year month weekday)
+                 (find-day schedule))]
+    [year month day]))
