b33dd49b996c5203e2d409227ed11f621a70001f
Federico Ravasio <ravasio.federico@gmail.com>
2015-10-03 21:25:45 +0200

    Improved atbash-cipher via nitpicks.

--- atbash-cipher/atbash_cipher.clj
+++ atbash-cipher/atbash_cipher.clj
@@ -7,12 +7,6 @@
 (defn- char-alpha? [ch]
   (java.lang.Character/isLetterOrDigit ch))
 
-(defn- chunks-of [n coll]
-  (if (<= (count coll) n)
-    (list coll)
-    (cons (take n coll)
-          (chunks-of n (drop n coll)))))
-
 (defn- invert [c]
   (if (char-digit? c)
     c
@@ -23,6 +17,6 @@
   (->> (str/lower-case plaintext)
        (filter char-alpha?)
        (map invert)
-       (chunks-of 5)
+       (partition-all 5)
        (map str/join)
        (str/join " ")))
